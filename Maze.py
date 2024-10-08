fa536f05a4548d1736bd0ca757b17b3a90920a32
Khin Kyal Sin Linn <21415300@student.curtin.edu.au>
2024-10-09 02:02:05 +0800

    Updated Maze.py with required changes

--- Maze.py
+++ Maze.py
@@ -92,21 +92,17 @@ if __name__ == "__main__":
             print("MESSAGE 2") # Out of bounds.
             
         else:
-            if grid[currentRow][currentCol] == EMPTY:
+           cell = grid[currentRow][currentCol] 
+           if cell == EMPTY:
                 grid[currentRow][currentCol] = VISITED
-                
-            elif grid[currentRow][currentCol] == WALL:
+           elif cell == WALL:
                 done = True
-                print("MESSAGE 3") # Hit wall.
-
-            elif grid[currentRow][currentCol] == END:
+                print("You stumble blindly into a solid concrete wall.")
+           elif cell == END:
                 done = True
-                solved = True
-                print("MESSAGE 4") # Solved.
-                
-            else:
+                solved = True print("SOLVED!")
+           else:
                 pass # Do nothing
-        
         charIndex += 1
     # end-while
 
